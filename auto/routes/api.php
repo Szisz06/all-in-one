2312642664ca393497ec6a0f125c2681f502f617
Szisz06 <118608773+Szisz06@users.noreply.github.com>
2024-04-17 17:18:46 +0200

    minden működik

--- auto/routes/api.php
+++ auto/routes/api.php
@@ -20,6 +20,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::resource('autos', AutoController::class);
+
 Route::get('autos', [AutoController::class, 'index']);
 
 Route::get('autos/{id}', [AutoController::class, 'show']); 
